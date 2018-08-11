42f344c47ba7abfcc11d5efaed5758275d0c9350
r3t2 <ravitejac@gmail.com>
2018-08-11 16:52:29 -0700

    reverse string excluding special chars

---
+++ algos/strings/reverse_excl_special.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_IN_STRING_SIZE 100
+
+static void swap_chars(char * str, uint32_t i, uint32_t j)
+{
+  char t = str[i];
+  str[i] = str[j];
+  str[j] = t;
+}
+
+static bool is_special(char c)
+{
+  bool ret_val = !( (c>='0' && c<='9') || 
+                    (c>='A' && c<='Z') || 
+                    (c>='a' && c<='z')  );
+  return ret_val;
+}
+void reverse_string_excl_special(char * str)
+{
+  if(str == NULL) return; // null string --> no work or ERROR/ASSERT
+  if(str[0] == '\0') return; // empty string --> no work
+
+  uint32_t st = 0, end = 0;
+  while(str[end+1] != '\0') end++; //variable end indexes the last char
+
+  while(st < end)
+  {
+    while((st<end) && (str[st]!='\0') && is_special(str[st]))   st++;
+    while((st<end) && (end>0)         && is_special(str[end]))  end--;
+    swap_chars(str, st, end);
+    st++; end--;
+  }
+  return ;
+}
+
+static void run_test(char * str)
+{
+  printf("input  = %s\n", str);
+  reverse_string_excl_special(str);
+  printf("output = %s\n\n", str);
+}
+int main(void)
+{
+  char * arg_str = malloc(MAX_IN_STRING_SIZE * sizeof(*arg_str));
+  char * str_lit = NULL;
+
+  str_lit=""; strcpy(arg_str, str_lit); run_test(arg_str);
+  str_lit="abc"; strcpy(arg_str, str_lit); run_test(arg_str);
+  str_lit="ab"; strcpy(arg_str, str_lit); run_test(arg_str);
+  str_lit="a"; strcpy(arg_str, str_lit); run_test(arg_str);
+  str_lit="a&b&c"; strcpy(arg_str, str_lit); run_test(arg_str);
+  str_lit="a&b&."; strcpy(arg_str, str_lit); run_test(arg_str);
+
+  return 0;
+}
