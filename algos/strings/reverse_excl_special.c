254710914a51b72420049e1eed2e7edae58b6d26
r3t2 <ravitejac@gmail.com>
2018-08-11 16:56:16 -0700

    implementation notes

--- algos/strings/reverse_excl_special.c
+++ algos/strings/reverse_excl_special.c
@@ -3,6 +3,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+Reverse an array without affecting special characters
+Given a string, that contains special character together with alphabets (‘a’ to ‘z’ and ‘A’ to ‘Z’), reverse the string in a way that special characters are not affected.
+
+Examples:
+
+Input:   str = "a,b$c"
+Output:  str = "c,b$a"
+Note that $ and , are not moved anywhere.  
+Only subsequence "abc" is reversed
+
+Input:   str = "Ab,c,de!$"
+Output:  str = "ed,c,bA!$"
+*/
 
 #define MAX_IN_STRING_SIZE 100
 
@@ -20,6 +34,10 @@ static bool is_special(char c)
                     (c>='a' && c<='z')  );
   return ret_val;
 }
+/**
+* In-place two-pass algorithm.
+* O(1) and O(N) space and time complexity.
+*/
 void reverse_string_excl_special(char * str)
 {
   if(str == NULL) return; // null string --> no work or ERROR/ASSERT
